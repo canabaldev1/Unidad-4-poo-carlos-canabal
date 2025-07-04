3e45e817cdff0530640229e5a3a40ebf11f18e31
canabaldev1 <canabal.dev1@gmail.com>
2025-07-03 19:29:35 -0500

    feat: carlos canabal - ejemplo 1 taller 1

---
+++ src/Taller1/Ejemplos/Persona.java
@@ -0,0 +1,16 @@
+package Taller1.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Persona {
+
+    String nombre;
+    
+    // Constructor
+    public Persona() {
+        nombre = "Sin nombre";
+    }
+
+}
