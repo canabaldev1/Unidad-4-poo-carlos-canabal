b8f2c697cc9d6d4abaf895c987c62fadf84ab410
canabaldev1 <canabal.dev1@gmail.com>
2025-07-03 19:32:14 -0500

    feat: carlos canabal - ejemplo 2.3 taller 1

--- src/Taller1/Ejemplos/Coche.java
+++ src/Taller1/Ejemplos/Coche.java
@@ -7,15 +7,22 @@ package Taller1.Ejemplos;
 public class Coche {
 
     String marca;
+    String modelo;
 
     // Constructor por defecto
     public Coche() {
         marca = "Desconocida";
     }
-    
+
     // Contructor con parámetros
     public Coche(String marca) {
         this.marca = marca;
+    }
+
+    // Constructor sobrecargado
+    public Coche(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
     }
 
 }
