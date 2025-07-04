3ba8bab3c69b69d6fc53ecc52a97f63fde54c372
canabaldev1 <canabal.dev1@gmail.com>
2025-07-03 21:18:38 -0500

    feat: carlos canabal - ejemplo 6.1 taller 3

---
+++ src/Taller3/Ejemplos/Banco.java
@@ -0,0 +1,19 @@
+package Taller3.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Banco {
+
+    public static double tasaInteres = 0.05;
+
+    public static void modificarTasa(double nuevaTasa) {
+        tasaInteres = nuevaTasa;
+    }
+
+    public static void cambiarSaldo(double saldo) {
+        saldo = saldo * (1 + tasaInteres);
+    }
+
+}
--- src/Taller3/Ejemplos/IntegradorEjemplosTaller3.java
+++ src/Taller3/Ejemplos/IntegradorEjemplosTaller3.java
@@ -12,14 +12,29 @@ public class IntegradorEjemplosTaller3 {
         Contador c3 = new Contador();
 // Se accede al método estático usando el nombre de la clase
         Contador.mostrarContador(); // Muestra: Contador global: 3
-        
+
         int resultado = Calculadora.sumar(5, 7);
-        System.out.println("Resultado: "+resultado);
-        
+        System.out.println("Resultado: " + resultado);
+
         try {
             Persona.mostrarNombre();
         } catch (Exception e) {
             System.out.println("Error al mostrar nombre");
         }
+
+        try {
+            Banco.modificarTasa(0.1);
+            System.out.println("Cambiada tasa con exito");
+        } catch (Exception e) {
+            System.out.println("Error al cambiar tasa de interes");
+        }
+
+        try {
+            Banco.cambiarSaldo(500);
+            System.out.println("Se cambia el saldo, pero es el mismo parámetro de la función, asi que esto no tiene sentido");
+        } catch (Exception e) {
+            System.out.println("Error al cambiar saldo");
+        }
+
     }
 }
