f48523c6f0ad055fbe5ebf26ba1704dd1dd4b452
canabaldev1 <canabal.dev1@gmail.com>
2025-07-03 21:55:49 -0500

    feat: carlos canabal - prueba ejemplo 4.1 taller 4

---
+++ src/Taller4/Ejemplos/Persona.java
@@ -0,0 +1,35 @@
+package Taller4.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Persona {
+
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if (edad >= 0) {
+            this.edad = edad;
+        }
+    }
+
+}
