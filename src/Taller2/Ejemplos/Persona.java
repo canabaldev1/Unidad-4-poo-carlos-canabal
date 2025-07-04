da8aecdff642489a3791ec3256188dbf6d5db6d4
canabaldev1 <canabal.dev1@gmail.com>
2025-07-03 20:26:58 -0500

    feat: carlos canabal - ejemplo 6.1 taller 2

--- src/Taller2/Ejemplos/Persona.java
+++ src/Taller2/Ejemplos/Persona.java
@@ -8,8 +8,13 @@ public class Persona {
 
     private String nombre;
 
-    public static void mostrarNombre() {
-        System.out.println(this.nombre);
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+        this.mostrarNombre();
+    }
+
+    public void mostrarNombre() {
+        System.out.println("Nombre: " + nombre);
     }
 
 }
