6ea38f438aada0f5804ad8f39835cf0e84e29f68
canabaldev1 <canabal.dev1@gmail.com>
2025-07-03 20:30:03 -0500

    feat: carlos canabal - ejercicio 1 taller 2

---
+++ src/Taller2/Ejercicios/Producto.java
@@ -0,0 +1,22 @@
+package Taller2.Ejercicios;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Producto {
+
+    private String nombre;
+    private double precio;
+
+    // Constructor
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public void mostrarProducto() {
+        System.out.println("Nombre del producto: " + nombre);
+        System.out.println("Precio: $" + precio);
+    }
+}
