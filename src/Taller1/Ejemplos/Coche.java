5806d0d75412ce17e2ff8f8fc07035ea91685c5f
canabaldev1 <canabal.dev1@gmail.com>
2025-07-03 19:30:40 -0500

    feat: carlos canabal - ejemplo 2.1 taller 1

---
+++ src/Taller1/Ejemplos/Coche.java
@@ -0,0 +1,16 @@
+package Taller1.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Coche {
+
+    String marca;
+
+    // Constructor por defecto
+    public Coche() {
+        marca = "Desconocida";
+    }
+
+}
