1793a6a6575327f98a710c0262921d0448dfabea
canabaldev1 <canabal.dev1@gmail.com>
2025-07-03 20:09:37 -0500

    feat: carlos canabal - ejercicio 3 taller 1

---
+++ src/Taller1/Ejercicios/Estudiante.java
@@ -0,0 +1,36 @@
+package Taller1.Ejercicios;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Estudiante {
+
+    private String nombre;
+    private int edad;
+    private String curso;
+
+    public Estudiante() {
+        this.nombre = "no definido";
+        this.edad = 0;
+        this.curso = "no definido";
+    }
+
+    public Estudiante(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public Estudiante(String nombre, int edad, String curso) {
+        this(nombre, edad);
+        this.curso = curso;
+    }
+
+    @Override
+    public String toString() {
+        return ("Cuenta Bancaria\n" + "--------------------------\n"
+                + "nombre:          " + nombre + "\n"
+                + "edad:            " + edad + "\n"
+                + "curso:           " + curso + "\n");
+    }
+}
