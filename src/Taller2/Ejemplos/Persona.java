8e2effc364b8a89316bb9b6cb0586f25efefc239
canabaldev1 <canabal.dev1@gmail.com>
2025-07-03 20:25:45 -0500

    feat: carlos canabal - ejemplo 5.1 taller 2

---
+++ src/Taller2/Ejemplos/Persona.java
@@ -0,0 +1,15 @@
+package Taller2.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Persona {
+
+    private String nombre;
+
+    public static void mostrarNombre() {
+        System.out.println(this.nombre);
+    }
+
+}
