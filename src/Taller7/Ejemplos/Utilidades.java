f38190ae85f199cef2f4e6f6195a6921f7b92f78
canabaldev1 <canabal.dev1@gmail.com>
2025-07-04 20:14:34 -0500

    feat: carlos canabal - prueba ejemplo 4.2 taller 7

---
+++ src/Taller7/Ejemplos/Utilidades.java
@@ -0,0 +1,13 @@
+package Taller7.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Utilidades {
+
+    public static void imprimirMensaje(String mensaje) {
+        System.out.println(mensaje);
+    }
+
+}
