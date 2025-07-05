1d3f597dca15ad07019eca84e0cb3233327e6ebd
canabaldev1 <canabal.dev1@gmail.com>
2025-07-05 15:47:15 -0500

    feat: carlos canabal - ejemplo 5.3 taller 15

---
+++ src/Taller15/Ejemplos/EmailService.java
@@ -0,0 +1,14 @@
+package Taller15.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class EmailService {
+
+    public void enviarEmail(String destinatario, String mensaje) {
+        System.out.println("Enviando email a: " + destinatario);
+        System.out.println("Mensaje: " + mensaje);
+    }
+
+}
---
+++ src/Taller15/Ejemplos/SMSService.java
@@ -0,0 +1,14 @@
+package Taller15.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class SMSService {
+
+    public void enviarSMS(String numero, String mensaje) {
+        System.out.println("Enviando SMS a: " + numero);
+        System.out.println("Mensaje: " + mensaje);
+    }
+
+}
