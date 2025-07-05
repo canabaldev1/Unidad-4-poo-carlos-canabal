2b71fa3b3d4f823816e92c031f4ed787116427f0
canabaldev1 <canabal.dev1@gmail.com>
2025-07-04 21:27:57 -0500

    feat: carlos canabal - prueba ejercicio 2 prueba taller 8

--- src/Taller8/Ejercicios/Persona.java
+++ src/Taller8/Ejercicios/Persona.java
@@ -6,4 +6,16 @@ package Taller8.Ejercicios;
  */
 public class Persona {
 
+    protected String nombre;
+    protected int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad + " años");
+    }
 }
--- src/Taller8/Ejercicios/PruebaTaller8.java
+++ src/Taller8/Ejercicios/PruebaTaller8.java
@@ -10,5 +10,9 @@ public class PruebaTaller8 {
         Coche coche = new Coche("Toyota Hilux", 220, 4);
         coche.mostrarInformacion();
 
+        Estudiante estudiante = new Estudiante("Carlos", 32, "tercer semestre");
+
+        estudiante.mostrarInformacion();
+
     }
 }
