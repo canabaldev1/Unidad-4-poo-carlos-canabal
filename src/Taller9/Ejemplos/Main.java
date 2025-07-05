a7f2e9dcd7b305fc7f78068a1dfe3fc5d2a10657
canabaldev1 <canabal.dev1@gmail.com>
2025-07-04 21:48:02 -0500

    feat: carlos canabal - ejemplo 6.1 taller 9

---
+++ src/Taller9/Ejemplos/Main.java
@@ -0,0 +1,11 @@
+package Taller9.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Main {
+    public static void main(String[] args) {
+        super.tipo = "Orquideas";
+    }
+}
---
+++ src/Taller9/Ejemplos/Planta.java
@@ -0,0 +1,15 @@
+package Taller9.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Planta {
+
+    private String tipo;
+
+    public Planta(String tipo) {
+        this.tipo = tipo;
+    }
+
+}
