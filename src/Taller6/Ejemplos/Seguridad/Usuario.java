a54a23fab239037aa3d5b5df8b745c3551b18116
canabaldev1 <canabal.dev1@gmail.com>
2025-07-04 19:21:00 -0500

    feat: carlos canabal - prueba ejemplo 6.1 taller 6

---
+++ src/Taller6/Ejemplos/Seguridad/Usuario.java
@@ -0,0 +1,15 @@
+package Taller6.Ejemplos.Seguridad;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Usuario {
+
+    protected String contraseña;
+
+    public Usuario(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+}
