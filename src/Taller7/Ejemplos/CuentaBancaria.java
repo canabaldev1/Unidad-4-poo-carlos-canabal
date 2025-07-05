296e5cdad08c004f80d15a5a057495703d0e5180
canabaldev1 <canabal.dev1@gmail.com>
2025-07-04 20:18:34 -0500

    feat: carlos canabal - prueba ejemplo 6.1 taller 7

---
+++ src/Taller7/Ejemplos/CuentaBancaria.java
@@ -0,0 +1,15 @@
+package Taller7.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class CuentaBancaria {
+
+    public double saldo;
+
+    public CuentaBancaria(double saldoInicial) {
+        saldo = saldoInicial;
+    }
+
+}
