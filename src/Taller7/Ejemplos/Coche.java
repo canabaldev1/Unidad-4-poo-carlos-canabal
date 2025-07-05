9636e10714ce84d7786d08928ab77c4a4f57589d
canabaldev1 <canabal.dev1@gmail.com>
2025-07-04 20:12:47 -0500

    feat: carlos canabal - prueba ejemplo 4.1 taller 7

---
+++ src/Taller7/Ejemplos/Coche.java
@@ -0,0 +1,30 @@
+package Taller7.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Coche {
+
+    private String marca;
+    private double velocidadMaxima;
+
+    public Coche(String marca, double velocidadMaxima) {
+        this.marca = marca;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setVelocidadMaxima(double velocidadMaxima) {
+        if (velocidadMaxima > 0) {
+            this.velocidadMaxima = velocidadMaxima;
+        }
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Marca: " + marca + ", Velocidad máxima: " + velocidadMaxima + " km/h");
+    }
+}
