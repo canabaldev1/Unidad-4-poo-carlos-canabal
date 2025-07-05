687ecaf437692068163665a4c0f846b754e0896a
canabaldev1 <canabal.dev1@gmail.com>
2025-07-04 19:19:46 -0500

    feat: carlos canabal - prueba ejemplo 5.1 taller 6

---
+++ src/Taller6/Ejemplos/Paquete1/Persona.java
@@ -0,0 +1,15 @@
+package Taller6.Ejemplos.Paquete1;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Persona {
+
+    protected String nombre;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
---
+++ src/Taller6/Ejemplos/Paquete2/Main.java
@@ -0,0 +1,13 @@
+package Taller6.Ejemplos.Paquete2;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Persona p = new Persona("Juan");
+        System.out.println(p.nombre);
+    }
+}
