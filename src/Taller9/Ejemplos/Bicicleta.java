2b5eac0fdbcbe7f9acaf11e72ad09d7d4cf7115a
canabaldev1 <canabal.dev1@gmail.com>
2025-07-04 21:50:04 -0500

    feat: carlos canabal - ejemplo 7.1 taller 9

---
+++ src/Taller9/Ejemplos/Bicicleta.java
@@ -0,0 +1,15 @@
+package Taller9.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Bicicleta extends Vehiculo {
+
+    @Override
+    public void mostrarTipo() {
+        super.mostrarTipo();
+        System.out.println("Este es un tipo de bicicleta.");
+    }
+
+}
---
+++ src/Taller9/Ejemplos/Vehiculo.java
@@ -0,0 +1,15 @@
+package Taller9.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Vehiculo {
+
+    protected String tipo;
+
+    public void mostrarTipo() {
+        System.out.println("Tipo de vehículo: " + tipo);
+    }
+
+}
