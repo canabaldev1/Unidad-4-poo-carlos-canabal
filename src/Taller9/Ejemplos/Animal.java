c7b833fbdba52c708b9d5e49974dd7925cca38f1
canabaldev1 <canabal.dev1@gmail.com>
2025-07-04 21:44:49 -0500

    feat: carlos canabal - ejemplo 5.1 taller 9

---
+++ src/Taller9/Ejemplos/Animal.java
@@ -0,0 +1,19 @@
+package Taller9.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Animal {
+
+    protected String especie;
+
+    public Animal(String especie) {
+        this.especie = especie;
+    }
+
+    public void mostrarEspecie() {
+        System.out.println("Especie: " + especie);
+    }
+
+}
