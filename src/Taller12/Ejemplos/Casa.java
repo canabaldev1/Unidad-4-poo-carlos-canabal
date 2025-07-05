628f619ed621bfe209067014e9ca76f709af9055
canabaldev1 <canabal.dev1@gmail.com>
2025-07-05 13:33:57 -0500

    feat: carlos canabal - ejemplo 7.2 taller 12

---
+++ src/Taller12/Ejemplos/Casa.java
@@ -0,0 +1,15 @@
+package Taller12.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Casa implements Movible {
+
+    @Override
+    public void moverse() {
+
+        System.out.println("Las casas no se mueven.");
+    }
+
+}
