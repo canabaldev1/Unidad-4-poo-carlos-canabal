55bbf71ac3c0aa08063e020c30577eb9517d471c
canabaldev1 <canabal.dev1@gmail.com>
2025-07-04 21:00:43 -0500

    feat: carlos canabal - prueba ejemplo 6.2 taller 8

---
+++ src/Taller8/Ejemplos/Coche.java
@@ -0,0 +1,17 @@
+package Taller8.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Coche extends Vehiculo {
+
+    public Coche(String marca) {
+        super(marca);
+    }
+
+    public void mostrarMarca() {
+        System.out.println(marca);
+    }
+
+}
---
+++ src/Taller8/Ejemplos/Vehiculo.java
@@ -0,0 +1,14 @@
+package Taller8.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Vehiculo {
+
+    private String marca;
+
+    public Vehiculo(String marca) {
+        this.marca = marca;
+    }
+}
