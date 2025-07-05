6354323dbacec0085348e28b9118c53e504dac47
canabaldev1 <canabal.dev1@gmail.com>
2025-07-04 22:33:52 -0500

    feat: carlos canabal - ejemplo 5.1 taller 10

---
+++ src/Taller10/Ejemplos/Empleado.java
@@ -0,0 +1,12 @@
+package Taller10.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Empleado {
+
+    public void trabajar() {
+        System.out.println("El empleado está trabajando.");
+    }
+}
