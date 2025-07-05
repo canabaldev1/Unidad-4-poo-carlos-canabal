f48238f6c405551491c565c61489fb55bf819eab
canabaldev1 <canabal.dev1@gmail.com>
2025-07-04 21:03:59 -0500

    feat: carlos canabal - prueba ejemplo 7.1 taller 8

--- src/Taller8/Ejemplos/Perro.java
+++ src/Taller8/Ejemplos/Perro.java
@@ -6,11 +6,9 @@ package Taller8.Ejemplos;
  */
 public class Perro extends Animal {
 
-    private String raza;
-
-    public Perro(String especie, String raza) {
-        super(especie); // Llama al constructor de la clase base
-        this.raza = raza;
+    public Perro(String raza) {
+        // super(especie);
+        System.out.println("Raza: "+raza);
     }
 
     public void ladrar() {
