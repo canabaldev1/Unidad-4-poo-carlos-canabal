6df613da8c0480df9c60ea6716cd89cdbea0a3ea
canabaldev1 <canabal.dev1@gmail.com>
2025-07-05 13:32:36 -0500

    feat: carlos canabal - ejemplo 7.1 taller 12

---
+++ src/Taller12/Ejemplos/Arbol.java
@@ -0,0 +1,13 @@
+package Taller12.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public class Arbol implements Corredor {
+
+    @Override
+    public void correr() {
+        System.out.println("Los árboles no pueden correr.");
+    }
+}
