82d4be7ca6a055563aa7e754774cd585b92d2999
canabaldev1 <canabal.dev1@gmail.com>
2025-07-05 14:10:06 -0500

    feat: carlos canabal - ejemplo 5.1 taller 13

---
+++ src/Taller13/Ejemplos/Figura.java
@@ -0,0 +1,14 @@
+package Taller13.Ejemplos;
+
+/**
+ *
+ * @author lm-carlos
+ */
+public abstract class Figura {
+
+    public abstract double calcularArea();
+
+    public void mostrarFigura() {
+        System.out.println("Mostrando figura");
+    }
+}
