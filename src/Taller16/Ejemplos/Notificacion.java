4f389b0ac012d3f107ec576773882558e7723e83
canabaldev1 <canabal.dev1@gmail.com>
2025-07-05 23:00:29 -0500

    feat: carlos canabal - ejemplo 6.2 taller 16

--- src/Taller16/Ejemplos/Notificacion.java
+++ src/Taller16/Ejemplos/Notificacion.java
@@ -8,4 +8,13 @@ public abstract class Notificacion {
 
     public abstract void enviar(String mensaje);
 
+    public void enviarConViolacion(String canal, String mensaje) {
+        if (canal.equals("Email")) {
+            System.out.println("Enviando email: " + mensaje);
+        } else if (canal.equals("SMS")) {
+            System.out.println("Enviando SMS: " + mensaje);
+        }
+// Problema: Si se agrega otro canal, hay que modificar este código.
+    }
+
 }
