9ccd191f1236fa393665ea21f1324aa5bfa2f644
canabaldev1 <canabal.dev1@gmail.com>
2025-07-05 22:59:58 -0500

    feat: carlos canabal - ejemplo 6.1 taller 16

--- src/Taller16/Ejemplos/ProcesadorPago.java
+++ src/Taller16/Ejemplos/ProcesadorPago.java
@@ -10,4 +10,13 @@ public class ProcesadorPago {
         metodoPago.procesarPago(monto);
     }
 
+    public void realizarPagoConViolacion(String metodo, double monto) {
+        if (metodo.equals("TarjetaCredito")) {
+            System.out.println("Procesando pago con tarjeta de crédito: $" + monto);
+        } else if (metodo.equals("PayPal")) {
+            System.out.println("Procesando pago con PayPal: $" + monto);
+        }
+// Problema: Si se agrega otro método, hay que modificar este código.
+    }
+
 }
